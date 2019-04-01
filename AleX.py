44344b75ab8fb01665f2297be94c11681a997957
Alexandre <alexandress12355@hotmail.com>
2019-04-02 00:12:56 +0200

    Loop, if < and >, number print

--- AleX.py
+++ AleX.py
@@ -1,3 +1,9 @@
+"""
+
+AleX interpreted language - Made by LEVEQUE Alexandre
+
+"""
+
 from sys import *
 import fnmatch
 
@@ -69,6 +75,28 @@ def lex(filecontents):
 				else:
 					tokens.append("EQUALS")
 				token=""
+			elif token == "<" and state == 0:
+				if expr != "" and isexpr == 0:
+					tokens.append("NUM:" + expr)
+					expr = ""
+				if var != "":
+					tokens.append("VAR:" + var)
+					var = ""
+					varStarted=0
+				else:
+					tokens.append("<")
+				token=""
+			elif token == ">" and state == 0:
+				if expr != "" and isexpr == 0:
+					tokens.append("NUM:" + expr)
+					expr = ""
+				if var != "":
+					tokens.append("VAR:" + var)
+					var = ""
+					varStarted=0
+				else:
+					tokens.append(">")
+				token=""
 			elif token == "|" and state == 0:
 				varStarted = 1
 				var += token
@@ -93,6 +121,18 @@ def lex(filecontents):
 			elif token.upper() == "IF":
 				tokens.append("IF")
 				token = ""
+			elif token.upper() == "WHILE":
+				tokens.append("WHILE")
+				token = ""
+			elif token.upper() == "ENDWHILE":
+				tokens.append("ENDWHILE")
+				token = ""
+			elif token.upper() == "<":
+				tokens.append("<")
+				token = ""
+			elif token.upper() == ">":
+				tokens.append(">")
+				token = ""
 			elif token.upper() == "THEN":
 				if expr != "" and isexpr == 0:
 					tokens.append("NUM:" + expr)
@@ -107,7 +147,7 @@ def lex(filecontents):
 					varStarted=0
 				tokens.append("THEN")
 				token = ""
-			elif token>="0" and token<="9﻿" :
+			elif token>="0" and token<="9﻿" and state!=1:
 				expr += token
 				token = ""
 			elif token in ["+","-","*","/","(",")","%"]:
@@ -131,7 +171,7 @@ def lex(filecontents):
 	
 def evalExpression(expr):
 	return (eval(expr, {'__builtins__':{}}))
-
+	
 def doPRINT(toPRINT):
 	if(toPRINT[0:6] == "STRING"):
 		toPRINT= toPRINT[8:-1]
@@ -160,11 +200,18 @@ def getINPUT(string, varname):
 
 def parse(tok):
 	i = 0
+	ir = 0
 	cond = 0
 	while(i < len(tok)):
-		if tok[i] == "ENDIF":
-			cond -= 1
+		if tok[i] == "ENDIF" :
+			if cond!=0:
+				cond -= 1
 			i+=1
+		elif tok[i] == "ENDWHILE" :	
+			if ir!=0:
+				i = ir
+			else :
+				i+=1
 		elif (tok[i] + " " + tok[i+1][0:6] == "PRINT STRING" or tok[i] + " " + tok[i+1][0:3] == "PRINT NUM" or tok[i] + " " + tok[i+1][0:4] == "PRINT EXPR" or tok[i] + " " + tok[i+1][0:3] == "PRINT VAR") :
 			if cond == 0 :
 				if tok[i+1][0:6] == "STRING" :
@@ -203,13 +250,110 @@ def parse(tok):
 			else:
 				cond += 1
 			i+=5
-		elif tok[i] + " " + tok[i+1][0:3] + " " + tok[i+2] + " " + tok[i+3][0:3] + " " + tok[i+4] == "IF NUM EQEQ VAR THEN" :
-			VAR=(getVAR(tok[i+3])) #Get the VAR Value
+		elif tok[i] + " " + tok[i+1][0:3] + " " + tok[i+2] + " " + tok[i+3][0:3] + " " + tok[i+4] == "IF NUM EQEQ VAR THEN" : #considering VAR comes from INPUT
+			VAR=(getVAR(tok[i+3])) #Get the VAR Value NUM
 			if tok[i+1][4:] == VAR[8:-1]:
 					cond = cond
 			else :
 					cond += 1
 			i+=5
+		elif tok[i] + " " + tok[i+1][0:3] + " " + tok[i+2] + " " + tok[i+3][0:3] + " " + tok[i+4] == "IF VAR EQEQ NUM THEN" : #considering VAR comes from INPUT
+			VAR=(getVAR(tok[i+1])) #Get the VAR Value NUM
+			if tok[i+3][4:] == VAR[8:-1]:
+					cond = cond
+			else :
+					cond += 1
+			i+=5
+		elif tok[i] + " " + tok[i+1][0:3] + " " + tok[i+2] + " " + tok[i+3][0:3] + " " + tok[i+4] == "IF VAR EQEQ VAR THEN" : #considering VAR comes from INPUT
+			VAR=(getVAR(tok[i+3])) #Get the VAR Value NUM
+			VAR2=(getVAR(tok[i+1]))
+			if VAR2[4:] == VAR[8:-1]:
+					cond = cond
+			else :
+					cond += 1
+			i+=5
+		elif tok[i] + " " + tok[i+1][0:3] + " " + tok[i+2] + " " + tok[i+3][0:3] + " " + tok[i+4] == "IF NUM < NUM THEN" :
+			tok4=int(tok[i+1][4:])
+			tok5=int(tok[i+3][4:])
+			if tok4 < tok5:
+				cond = cond
+			else:
+				cond += 1
+			i+=5
+		elif tok[i] + " " + tok[i+1][0:3] + " " + tok[i+2] + " " + tok[i+3][0:3] + " " + tok[i+4] == "IF NUM < VAR THEN" : #considering VAR comes from INPUT
+			VAR=(getVAR(tok[i+3])) #Get the VAR Value NUM
+			VAR=int(VAR[8:-1])
+			tok2=int(tok[i+1][4:])
+			if tok2 < VAR :
+					cond = cond
+			else :
+					cond += 1
+			i+=5
+		elif tok[i] + " " + tok[i+1][0:3] + " " + tok[i+2] + " " + tok[i+3][0:3] + " " + tok[i+4] == "IF VAR < NUM THEN" : #considering VAR comes from INPUT
+			VAR=(getVAR(tok[i+1])) #Get the VAR Value NUM
+			VAR=int(VAR[8:-1])
+			tok3=int(tok[i+3][4:])
+			if VAR < tok3:
+					cond = cond
+			else :
+					cond += 1
+			i+=5
+		elif tok[i] + " " + tok[i+1][0:3] + " " + tok[i+2] + " " + tok[i+3][0:3] + " " + tok[i+4] == "IF VAR < VAR THEN" : #considering VAR comes from INPUT
+			VAR=(getVAR(tok[i+3])) #Get the VAR Value NUM
+			VAR2=(getVAR(tok[i+1]))
+			VAR=int(VAR[8:-1])
+			VAR2=int(VAR2[4:])
+			if VAR2 < VAR:
+					cond = cond
+			else :
+					cond += 1
+			i+=5
+		elif tok[i] + " " + tok[i+1][0:3] + " " + tok[i+2] + " " + tok[i+3][0:3] + " " + tok[i+4] == "IF NUM > NUM THEN" :
+			tok6=int(tok[i+1][4:])
+			tok7=int(tok[i+3][4:])
+			if tok6 > tok7:
+				cond = cond
+			else:
+				cond += 1
+			i+=5
+		elif tok[i] + " " + tok[i+1][0:3] + " " + tok[i+2] + " " + tok[i+3][0:3] + " " + tok[i+4] == "IF NUM > VAR THEN" : #considering VAR comes from INPUT
+			VAR=(getVAR(tok[i+3])) #Get the VAR Value NUM
+			VAR=int(VAR[8:-1])
+			tok8=int(tok[i+1][4:])
+			if tok8 > VAR:
+					cond = cond
+			else :
+					cond += 1
+			i+=5
+		elif tok[i] + " " + tok[i+1][0:3] + " " + tok[i+2] + " " + tok[i+3][0:3] + " " + tok[i+4] == "IF VAR > NUM THEN" : #considering VAR comes from INPUT
+			VAR=(getVAR(tok[i+1])) #Get the VAR Value NUM
+			VAR=int(VAR[8:-1])
+			tok1=int(tok[i+3][4:])
+			if VAR > tok1 :
+					cond = cond
+			else :
+					cond += 1
+			i+=5
+		elif tok[i] + " " + tok[i+1][0:3] + " " + tok[i+2] + " " + tok[i+3][0:3] + " " + tok[i+4] == "IF VAR > VAR THEN" : #considering VAR comes from INPUT
+			VAR=(getVAR(tok[i+3])) #Get the VAR Value INPUT
+			VAR2=(getVAR(tok[i+1])) #NOT INPUT
+			VAR=int(VAR[8:-1])
+			VAR2=int(VAR2[4:])
+			if VAR2 > VAR:
+					cond = cond
+			else :
+					cond += 1
+			i+=5
+		elif tok[i] + " " + tok[i+1][0:3] + " " + tok[i+2] + " " + tok[i+3][0:3] + " " + tok[i+4] == "WHILE VAR EQEQ NUM THEN" : #considering VAR not INPUT
+			VAR=(getVAR(tok[i+1])) #Get the VAR Value NUM
+			tok9=int(tok[i+3][4:])
+			VAR=int(VAR[4:])
+			if tok9 == VAR :
+				ir = i
+			else :
+				ir = 0
+			i+=5
+
 
 
 def run():
